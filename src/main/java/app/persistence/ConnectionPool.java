c1c154c5ad4d3add7d89955ac5234411caeb7564
Tesselation93 <tessvanslyckspam@gmail.com>
2025-10-22 11:56:40 +0200

    Refaktoreret ConnectionPool og fikset config håndtering

--- src/main/java/app/persistence/ConnectionPool.java
+++ src/main/java/app/persistence/ConnectionPool.java
@@ -3,7 +3,6 @@ package app.persistence;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,26 +12,19 @@ public class ConnectionPool {
     private static HikariDataSource dataSource;
 
     static {
-        try (InputStream in = ConnectionPool.class
-                .getClassLoader()
-                .getResourceAsStream("config.properties")) {
-
+        try (InputStream in = ConnectionPool.class.getClassLoader().getResourceAsStream("config.properties")) {
             Properties p = new Properties();
-            if (in == null) {
-                throw new RuntimeException("config.properties ikke fundet");
-            }
+            if (in == null) throw new RuntimeException("config.properties ikke fundet");
             p.load(in);
 
             String url = p.getProperty("db.url");
             String username = p.getProperty("db.user");
             String password = p.getProperty("db.password");
-            String driver = p.getProperty("db.driver", "org.postgresql.Driver");
 
-            HikariConfig cfg = new HikariConfig(p);
+            HikariConfig cfg = new HikariConfig();
             cfg.setJdbcUrl(url);
             cfg.setUsername(username);
             cfg.setPassword(password);
-            cfg.setDriverClassName(driver);
             cfg.setMaximumPoolSize(5);
             cfg.setPoolName("CupcakePool");
 
