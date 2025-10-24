bf078ef23c501fad5a1d4a05cda6e29a37c1d643
Tesselation93 <tessvanslyckspam@gmail.com>
2025-10-24 12:17:23 +0200

    Topping klassen tilføjet

---
+++ src/main/java/app/entities/Topping.java
@@ -0,0 +1,35 @@
+package app.entities;
+
+public class Topping {
+    private int topId;
+    private String navn;
+    private double pris;
+    private boolean aktiv;
+
+    public Topping(int id, String navn, double pris, boolean aktiv) {
+        this.topId = id;
+        this.navn = navn;
+        this.pris = pris;
+        this.aktiv = aktiv;
+    }
+    public int getTopId() {
+        return topId;
+    }
+    public String getNavn() {
+        return navn;
+    }
+    public double getPris() {
+        return pris;
+    }
+    public boolean isAktiv() {return aktiv; }
+
+    @Override
+    public String toString() {
+        return "Topping{" +
+                "topId=" + topId +
+                ", navn=" + navn +
+                ", pris=" + pris +
+                ", aktiv=" + aktiv +
+                '}';
+    }
+}
