a37011d9a477cf51f0eb65583d76a98df880cdd7
Tesselation93 <tessvanslyckspam@gmail.com>
2025-10-27 14:32:14 +0100

    Tilføjet oprettelse og rettet på layout

--- src/main/java/app/entities/Bruger.java
+++ src/main/java/app/entities/Bruger.java
@@ -6,28 +6,43 @@ public class Bruger {
     private String kodeord;
     private String rolle;
     private double saldo;
+    private boolean oprettet;
 
 
-    public Bruger(int brugerId, String email, String kodeord, String rolle, double saldo) {
+    public Bruger(int brugerId, String email, String kodeord, String rolle, double saldo, boolean oprettet) {
         this.brugerId = brugerId;
         this.email = email;
         this.kodeord = kodeord;
         this.rolle = rolle;
         this.saldo = saldo;
+        this.oprettet = oprettet;
     }
     public int getBrugerId() {
+
         return brugerId;
     }
     public String getEmail() {
+
         return email;
     }
     public String getKodeord() {
+
         return kodeord;
     }
     public String getRolle() {
+
         return rolle;
     }
+    public double getSaldo() {
+
+        return saldo;
+    }
+    public boolean isOprettet() {
+
+        return oprettet;
+    }
     public boolean erAdmin() {
+
         return rolle != null && rolle.equalsIgnoreCase("administrator");
     }
 
@@ -38,6 +53,7 @@ public class Bruger {
                 ", email=" + email +
                 ", rolle=" + rolle +
                 ", saldo=" + saldo +
+                ", oprettet=" + oprettet +
                 '}';
     }
 }
